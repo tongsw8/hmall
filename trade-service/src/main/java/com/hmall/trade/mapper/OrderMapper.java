fba5140944bab43034954554dbf3c543c7a2dc31
tongsw <shuaiweitong@gmail.com>
2026-05-11 16:10:12 +0800

    feat(user): 完成交易模块拆分

---
+++ trade-service/src/main/java/com/hmall/trade/mapper/OrderMapper.java
@@ -0,0 +1,8 @@
+package com.hmall.trade.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hmall.trade.domain.po.Order;
+
+public interface OrderMapper extends BaseMapper<Order> {
+
+}
