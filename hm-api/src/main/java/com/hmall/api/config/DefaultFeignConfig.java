e303d111bbb61a669986924650d2cf8127e349fe
tongsw <shuaiweitong@gmail.com>
2026-05-14 13:29:04 +0800

    feat(api): 实现微服务 到 微服务中间的用户信息传递
    - 集成UserContext工具类用于获取用户信息
    - 实现RequestInterceptor接口创建用户信息请求拦截器
    - 在请求头中添加user字段传递用户ID
    - 添加hm-common依赖支持用户上下文功能

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,6 +1,9 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 
@@ -8,5 +11,16 @@ public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                Long user = UserContext.getUser();
+                requestTemplate.header("user", user.toString());
+            }
+        };
     }
 }
