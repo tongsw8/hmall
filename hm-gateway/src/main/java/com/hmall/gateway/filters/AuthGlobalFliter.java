17eb46a6398f22e273588e833220768fe8465cbe
tongsw <shuaiweitong@gmail.com>
2026-05-14 13:06:59 +0800

    feat(gateway): 添加自定义过滤器实现登录校验
    - 添加网关服务的application.yaml配置文件，包含Nacos注册中心配置和路由规则
    - 实现AuthGlobalFilter全局认证过滤器，集成JWT令牌验证功能
    - 添加AuthProperties和JwtProperties配置类用于认证相关配置
    - 创建JwtTool工具类实现JWT令牌的创建和解析功能
    - 配置SecurityConfig包含密码编码器和RSA密钥对初始化
    - 添加.gitignore文件排除IDE配置和编译产物等不需要提交的文件

---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/AuthGlobalFliter.java
@@ -0,0 +1,69 @@
+package com.hmall.gateway.filters;
+
+import com.hmall.gateway.config.AuthProperties;
+import com.hmall.gateway.utils.JwtTool;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class AuthGlobalFliter implements GlobalFilter, Ordered {
+
+    private final AuthProperties authProperties;
+
+    private final JwtTool jwtTool;
+
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        if (isExcludePath(request.getPath().toString())) {
+            // 放行
+            return chain.filter(exchange);
+        }
+        String token = null;
+        List<String> authorization = request.getHeaders().get("authorization");
+        if (authorization == null || authorization.size() == 0) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        Long userId;
+        try {
+            userId = jwtTool.parseToken(authorization.get(0));
+        } catch (Exception e) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        // TODO 传递用户信息
+
+        // 放行
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+    private boolean isExcludePath(String path) {
+        for (String excludePath : authProperties.getExcludePaths()) {
+            if (antPathMatcher.match(excludePath, path)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
