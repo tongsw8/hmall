9e73aa4db29cf4a134ffeed247a6f20448d4120d
tongsw <shuaiweitong@gmail.com>
2026-05-20 18:19:59 +0800

    fix(comment): 解决自动配置依赖缺失问题

--- hm-common/src/main/java/com/hmall/common/config/MqConfig.java
+++ hm-common/src/main/java/com/hmall/common/config/MqConfig.java
@@ -2,10 +2,12 @@ package com.hmall.common.config;
 
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(MessageConverter.class)
 public class MqConfig {
     @Bean
     public MessageConverter messageConverter() {
